39a7592760140314be00c156fd10c55eb73a5c1f
lrpina <pina.laura@gmail.com>
2017-03-21 10:19:06 -0700

    got single page to work by passing 1 but still hard coded

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -46,12 +46,12 @@ angular
         controller: 'FamilydailyviewCtrl',
         controllerAs: 'familydailyview'
       })
-      .when('/sdview', {
+      .when('/sdview/1', {
         templateUrl: 'views/sdview.html',
         controller: 'SdviewCtrl',
         controllerAs: 'sdview'
       })
-      .otherwise({
+      .otherwise({ //I want to add an error page when we don't get to the right page
         redirectTo: '/'
       });
   });
