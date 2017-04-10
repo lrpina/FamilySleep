8b3c7ed9970f9ad8e901ee24cdeb79bae9da599a
lrpina <pina.laura@gmail.com>
2017-04-10 14:48:29 -0700

    testing al merges and installing momentjs

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -36,6 +36,8 @@ module.exports = function(config) {
       'bower_components/angular-chart.js/dist/angular-chart.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/recordrtc/RecordRTC.js',
+      'bower_components/moment/moment.js',
+      'bower_components/angular-momentjs/angular-momentjs.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
