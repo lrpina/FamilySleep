6377d8ad504c0c3d2c16a18f1ef7ea26dfa7a831
lrpina <pina.laura@gmail.com>
2017-05-05 10:03:24 -0700

    created factory to maintain self-report state

---
+++ client/app/scripts/services/selfreportstate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name FamilySleep.selfReportState
+ * @description
+ * # selfReportState
+ * Factory in the FamilySleep.
+ * Returning an empty factory
+ */
+angular.module('FamilySleep')
+  .factory('selfReportState', function () {
+    return {};
+  });
